a4e7699e28f2e68077ed9bd499cdf9817f609671
omkardhangar <33811033+omkardhangar@users.noreply.github.com>
2020-09-25 13:33:48 +0530

    uploaded

---
+++ facebooklogin/src/test/java/com/omkar/facebooklogin/FacebookloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omkar.facebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
